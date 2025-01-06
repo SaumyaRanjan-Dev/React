5d12ef33349e092f9740284383ae240a77a58c4e
Saumya Ranjan Mohapatra <saumyaranjanmohapatra978@gmail.com>
2025-01-06 20:49:09 +0530

    Create App.jsx

---
+++ ReactNative/Day0/App.jsx
@@ -0,0 +1,15 @@
+//Stater Plate
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
