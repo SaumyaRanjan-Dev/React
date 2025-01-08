33b243360c74feb5105629e91eb12c662d06909e
Saumya Ranjan Mohapatra <saumyaranjanmohapatra978@gmail.com>
2025-01-08 15:21:20 +0530

    Create Home.jsx

---
+++ ReactNative/Day5/src/Screens/Home.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react'
+
+const Home = ({navigation}) => {
+  return (
+    <View style={{width:"100%",height:"100%",justifyContent:"center",alignItems:"center"}}>
+      <Text style={{fontSize:20,fontWeight:"500",marginBottom:10}}>Home</Text>
+      <Button title="Profile"  onPress={()=> navigation.navigate("Profile",{id:1,name:"Lulu"})}    />
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
