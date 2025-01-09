df6785b6a7d23da3e4bf7bfedfb374d44872ee86
Saumya Ranjan Mohapatra <saumyaranjanmohapatra978@gmail.com>
2025-01-09 20:07:18 +0530

    Create App.jsx

---
+++ ReactNative/Project01/App.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import HomeScreen from './src/Screens/HomeScreen'
+
+const App = () => {
+  return (
+    <View>
+      <HomeScreen/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
